25f0e03d73ad3e6a7a73a6821f07290ce270d1f1
dsjang <dsjang>
2023-04-04 15:53:46 +0900

    no message

---
+++ sb2-multi-template/sb2-multi-template-cmmnlib/src/main/java/com/example/cmmnlib/SpringBoot2MultiTemplateApplication.java
@@ -0,0 +1,37 @@
+package com.example.cmmnlib;
+
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.web.client.HttpClientMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JndiDataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
+//spring-boot에서 root-context와 child-context 구분은 의미 없음
+@ComponentScan(
+  basePackages = {"com.example"}
+//, excludeFilters = @ComponentScan.Filter(classes = {Controller.class, RestController.class})
+)
+@ConfigurationPropertiesScan(basePackages = {"com.example"}) // @ConfigurationProperties Scan 설정
+@ServletComponentScan       (basePackages = {"com.example"}) // @WebFilter, @WebListener, @WebServlet annotation 사용하기 위한 annotation
+@EnableAutoConfiguration(exclude= {
+    DataSourceAutoConfiguration.class // dataSource yml 설정시 제거
+  , MybatisAutoConfiguration.class    // mybatis yml 설정시 제거
+  , DataSourceTransactionManagerAutoConfiguration.class
+  , JndiDataSourceAutoConfiguration.class
+  , SecurityAutoConfiguration.class
+  , ManagementWebSecurityAutoConfiguration.class
+  , JacksonAutoConfiguration.class
+  , HttpClientMetricsAutoConfiguration.class // spring-boot-actuator-autoconfigure
+})
+@SpringBootApplication
+public class SpringBoot2MultiTemplateApplication {
+
+}
---
+++ sb2-multi-template/sb2-multi-template-web/src/main/java/com/example/web/SpringBoot2MultiTemplateWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+
+import com.example.cmmnlib.SpringBoot2MultiTemplateApplication;
+
+public class SpringBoot2MultiTemplateWebApplication extends SpringBoot2MultiTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2MultiTemplateWebApplication.class, args);
+    }
+
+}
